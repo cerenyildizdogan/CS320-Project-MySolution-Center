cdebada888f4ab344bb5fd94205c2bb94c415407
melikeakal <80429026+melikeakal@users.noreply.github.com>
2021-12-24 15:35:25 +0300

    Create Salter.java
    Salter.java has been created for hashing passwords

---
+++ cs320-project/src/main/java/edu/ozu/cs320project/Salter.java
@@ -0,0 +1,36 @@
+package edu.ozu.cs320project;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class Salter {
+    public static void main(String[] args)
+    {
+        System.out.println(">: " + salt("do123", "CS320Project"));
+    }
+
+    public static String salt(String A, String B)
+    {
+        byte[] hash = new byte[0];
+
+        try {
+            KeySpec spec = new PBEKeySpec(
+                    A.toCharArray(),
+                    B.getBytes(StandardCharsets.UTF_8),
+                    65536,
+                    128
+            );
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            hash = factory.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+
+        return Base64.getEncoder().encodeToString(hash);
+    }
+}
