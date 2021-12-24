4b65390201a1e4a77e606d3e86582fee874e52ef
melikeakal <80429026+melikeakal@users.noreply.github.com>
2021-12-24 15:26:03 +0300

    Update Database.java

--- cs320-project/src/main/java/edu/ozu/cs320project/configurations/Database.java
+++ cs320-project/src/main/java/edu/ozu/cs320project/configurations/Database.java
@@ -1,8 +1,25 @@
 package edu.ozu.cs320project.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class Database {
+
+    @Bean
+    public DataSource MySQLDataSource(){
+
+        DriverManagerDataSource source = new DriverManagerDataSource();
+
+        source.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        source.setUrl("jdbc:mysql://localhost:3306/cs320");
+        source.setUsername("root");
+        source.setPassword("kerem2008");
+
+        return source;
+    }
 
 }
