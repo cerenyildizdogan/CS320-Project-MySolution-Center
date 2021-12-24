4d12413e0d775b5a56eb9649fd6978d0fd42e8f7
cerenyildizdogan <96374502+cerenyildizdogan@users.noreply.github.com>
2021-12-24 16:26:52 +0300

    Update AppController.java
    @GetMapping("/logout") has been added

--- cs320-project/src/main/java/edu/ozu/cs320project/controllers/AppController.java
+++ cs320-project/src/main/java/edu/ozu/cs320project/controllers/AppController.java
@@ -1,5 +1,6 @@
 package edu.ozu.cs320project.controllers;
 
+
 import edu.ozu.cs320project.Salter;
 import edu.ozu.cs320project.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
-@SessionAttributes({"username","password","level","usertypename","id", "email"})
+
+@SessionAttributes({"username","password","level","usertypename","id","email"})
 public class AppController {
 
     @Autowired
@@ -19,33 +23,33 @@ public class AppController {
     JdbcTemplate conn;
 
     @GetMapping("/")
-    public String index(ModelMap model){
+    public String index(ModelMap model) {
         return "index";
     }
 
     @GetMapping("/login")
-    public String loginPage(ModelMap model){
+    public String loginPage(ModelMap model) {
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(ModelMap model, @RequestParam String username, @RequestParam String password){
+    public String login(ModelMap model, @RequestParam String username, @RequestParam String password) {
 
         password = Salter.salt(password, "CS320Project");
 
-        if(!service.validate(username,password)){
-            model.put("errorMessage","Invalid Credentials");
+        if (!service.validate(username, password)) {
+            model.put("errorMessage", "Invalid Credentials");
             return "login";
         }
-        
         Integer usertypeid = service.getUserType(username, password);
-        String usertypename = service.getUserTypeName(username,password);
-        String name = service.getUsername(username,password);
+        String usertypename = service.getUserTypeName(username, password);
+        String name = service.getUsername(username, password);
         int userid = service.getUserID(username);
-        
+
+
         model.put("id", usertypeid.toString());
-        model.put("usertypename",usertypename);
-        model.put("userid",userid);
+        model.put("usertypename", usertypename);
+        model.put("userid", userid);
         model.put("username", name);
 
         if (usertypeid == 1) {
@@ -58,3 +62,13 @@ public class AppController {
         }
 
     }
+
+    @GetMapping("/logout")
+    public String logout(ModelMap model, WebRequest request, SessionStatus session) {
+        session.setComplete();
+        request.removeAttribute("username", WebRequest.SCOPE_SESSION);
+
+        return "redirect:/login";
+    }
+
+}
