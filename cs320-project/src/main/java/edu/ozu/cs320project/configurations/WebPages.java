2dd27236576ecb6d229b67ffd86d4db2dedd0373
melikeakal <80429026+melikeakal@users.noreply.github.com>
2021-12-24 15:26:29 +0300

    Update WebPages.java

--- cs320-project/src/main/java/edu/ozu/cs320project/configurations/WebPages.java
+++ cs320-project/src/main/java/edu/ozu/cs320project/configurations/WebPages.java
@@ -1,8 +1,20 @@
 package edu.ozu.cs320project.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebPages {
 
+    @Bean
+    public ViewResolver JSPViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
 }
