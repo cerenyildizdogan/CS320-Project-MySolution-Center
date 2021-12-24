797880bc4b73ee9d9c8a1978b58d6050ffd50708
melikeakal <80429026+melikeakal@users.noreply.github.com>
2021-12-24 15:36:56 +0300

    Update AppController.java
    @GetMapping("/") has been added

--- cs320-project/src/main/java/edu/ozu/cs320project/controllers/AppController.java
+++ cs320-project/src/main/java/edu/ozu/cs320project/controllers/AppController.java
@@ -1,8 +1,15 @@
 package edu.ozu.cs320project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AppController {
+
+    @GetMapping("/")
+    public String index(ModelMap model) {
+        return "index";
+    }
 
 }
