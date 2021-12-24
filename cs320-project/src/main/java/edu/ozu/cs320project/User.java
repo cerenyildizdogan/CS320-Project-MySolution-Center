28b67a40f771a4741d4f28cfa5f1545ed470dfd2
melikeakal <80429026+melikeakal@users.noreply.github.com>
2021-12-24 15:31:12 +0300

    Create User.java

---
+++ cs320-project/src/main/java/edu/ozu/cs320project/User.java
@@ -0,0 +1,40 @@
+package edu.ozu.cs320project;
+
+public class User {
+    int usertypeid;
+    int userid;
+    String username;
+    String type;
+
+    public int getUsertypeid() {
+        return usertypeid;
+    }
+
+    public void setUsertypeid(int usertypeid) {
+        this.usertypeid = usertypeid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getUserid() {
+        return userid;
+    }
+
+    public void setUserid(int userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
