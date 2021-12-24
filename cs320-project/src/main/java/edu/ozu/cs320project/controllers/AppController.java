b1f42a12ae805653b0548b68dad9822b2c0d8749
cerenyildizdogan <96374502+cerenyildizdogan@users.noreply.github.com>
2021-12-24 16:18:14 +0300

    Update AppController.java
    @GetMapping("/login") and @PostMapping("/login") have been added

--- cs320-project/src/main/java/edu/ozu/cs320project/controllers/AppController.java
+++ cs320-project/src/main/java/edu/ozu/cs320project/controllers/AppController.java
@@ -1,15 +1,60 @@
 package edu.ozu.cs320project.controllers;
 
+import edu.ozu.cs320project.Salter;
+import edu.ozu.cs320project.services.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
+@SessionAttributes({"username","password","level","usertypename","id", "email"})
 public class AppController {
 
+    @Autowired
+    LoginService service;
+
+    @Autowired
+    JdbcTemplate conn;
+
     @GetMapping("/")
-    public String index(ModelMap model) {
+    public String index(ModelMap model){
         return "index";
     }
 
-}
+    @GetMapping("/login")
+    public String loginPage(ModelMap model){
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(ModelMap model, @RequestParam String username, @RequestParam String password){
+
+        password = Salter.salt(password, "CS320Project");
+
+        if(!service.validate(username,password)){
+            model.put("errorMessage","Invalid Credentials");
+            return "login";
+        }
+        
+        Integer usertypeid = service.getUserType(username, password);
+        String usertypename = service.getUserTypeName(username,password);
+        String name = service.getUsername(username,password);
+        int userid = service.getUserID(username);
+        
+        model.put("id", usertypeid.toString());
+        model.put("usertypename",usertypename);
+        model.put("userid",userid);
+        model.put("username", name);
+
+        if (usertypeid == 1) {
+            return "redirect:/residentPage";
+        }
+        if (usertypeid == 2) {
+            return "redirect:/doPage";
+        } else {
+            return "login";
+        }
+
+    }
